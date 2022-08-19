176bd7c7078043d079ac77afc70cab1485882b26
Mehedi Hasan Opi Sarker <opisarker99@gmail.com>
2022-08-19 10:18:55 +0600

    add react icon and other component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { InputFeild } from './componets/InputFeild';
+import { Todo } from './componets/model';
+import TodoList from './componets/TodoList';
 
-const App:React.FC=()=> {
-
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }])
+      setTodo("");
+    }
+  };
   return (
     <div className="App">
       <span className='heading'>Taskify</span>
-      <InputFeild></InputFeild>
+      <InputFeild todo={todo} setTodo={setTodo}
+        handleAdd={handleAdd}
+      ></InputFeild>
+     
+    <TodoList todos={todos} setTodos={setTodos}/>
+
     </div>
   );
 }
--- src/componets/InputFeild.tsx
+++ src/componets/InputFeild.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './InputFeild.css';
 
-export const InputFeild = () => {
+interface Props {
+  todo: string,
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+export const InputFeild: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef=useRef<HTMLInputElement>(null);
   return (
     <div>
-      <form className='input'>
-        <input type="input" name="" placeholder='Enter a task' className='input_box' id="" />
-        <button className='input_submit' type='submit'>Go</button>
+      <form className='input' onSubmit={(e)=>{
+        handleAdd(e);
+        inputRef.current?.blur();
+        
+        }}> 
+        <input type="input" name="" 
+          value={todo} 
+          ref={inputRef}
+          onChange={(e) => setTodo(e.target.value)} 
+          placeholder='Enter a task'
+          className='input_box' id="" />
+        <button className='input_submit'
+          type='submit'>Go</button>
       </form>
     </div>
-
   )
 }
